b496b200f7bc463da74c64d5a4f7764bb814130d
eda3 <gerorin1010@gmail.com>
2020-07-27 20:51:45 +0900

    commit

--- itp1_1_b/src/main.rs
+++ itp1_1_b/src/main.rs
@@ -1,11 +1,19 @@
 use std::io;
-use std::str::FromStr;
+
+// 一行読み込み
+fn input() -> u32 {
+    let mut buf = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buf).unwrap();
+    return buf.trim().parse().unwrap();
+}
+
+// 三乗して返却
+fn cube(x: u32) -> u32 {
+    return x * x * x;
+}
 
 fn main() {
-    let stdin = io::stdin();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf).ok();
-    let mut it = buf.split_whitespace().map(|n| usize::from_str(n).unwrap());
-    let (n, m) = (it.next().unwrap(), it.next().unwrap());
-    println!("N: {}, M: {}", n, m);
+    let x: u32 = input();
+    println!("{}", cube(x));
 }
