9e8edcaab1275910250ad77fe97e6519e244f660
eda3 <gerorin1010@gmail.com>
2020-07-28 06:45:04 +0900

    commt

--- 01_itp1/itp1_1_d_Watch/src/main.rs
+++ 01_itp1/itp1_1_d_Watch/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s = input();
+    let h = s / 3600;
+    let m = (s - h * 3600) / 60;
+    let s = s % 60;
+    println!("{}:{}:{}", h, m, s);
 }
 
-rsin\
+
+fn input() -> u32 {
+    let mut buf = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut buf).unwrap();
+    return buf.trim().parse().unwrap();
+}
