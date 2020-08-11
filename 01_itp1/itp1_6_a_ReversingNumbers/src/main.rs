2b5af1a3dbcca963c92cc4f616a1c29af362365d
eda3 <gerorin1010@gmail.com>
2020-08-11 20:03:05 +0900

    commit

--- 01_itp1/itp1_6_a_ReversingNumbers/src/main.rs
+++ 01_itp1/itp1_6_a_ReversingNumbers/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     let z: Vec<i32> = r();
-    let v: Vec<i32> = r();
+    let ut v: Vec<i32> = r();
     println!("{}", v.iter().map(std::string::ToString::to_string).collect::<Vec<_>>().join(" "));
 }
-
-fn r<T: std::str::FromStr>() -> Vec<T> {
-    let mut b = String::new();
-    let s = std::io::stdin();
+fn r<T:std::str::FromStr>()->Vec<T>{
+    let mut b=String::new();
+    let s=std::io::stdin();
     s.read_line(&mut b).unwrap();
-    b.split_whitespace().rev().flat_map(str::parse).collect()
+    b.split_whitespace().flat_map(str::parse).collect()
 }
