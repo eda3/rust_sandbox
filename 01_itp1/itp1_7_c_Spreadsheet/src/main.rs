0a51fe78f433da7294600e2c07b0908bb26a26c1
eda3 <gerorin1010@gmail.com>
2020-08-19 16:45:35 +0900

    commit

--- 01_itp1/itp1_7_c_Spreadsheet/src/main.rs
+++ 01_itp1/itp1_7_c_Spreadsheet/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+ let v:Vec<usize>=z();
+ let (r,c)=(v[0],v[1]);
+ let mut s=vec![0;c+1];
+ for _ in 0..r{
+  let v:Vec<isize>=z();
+  y(&v);
+  let t=v.iter().sum::<isize>();
+  println!(" {}",t);
+  for (x,y) in v.iter().enumerate(){s[x]=s[x]+y}
+  s[c]+=t;
+ }
+ y(&s);
+ println!();
+}
+fn z<T:std::str::FromStr>()->Vec<T>{
+ let mut b=String::new();
+ let s=std::io::stdin();
+ s.read_line(&mut b).unwrap();
+ b.split_whitespace().flat_map(str::parse).collect()
+}
+fn y(v:&Vec<isize>)->(){
+ print!("{}",v.iter().map(std::string::ToString::to_string).collect::<Vec<_>>().join(" "));
 }
