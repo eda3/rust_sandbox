1fb2544fb049d0ed46db8d85f6e1fc4e5aaceaa0
eda3 <gerorin1010@gmail.com>
2020-08-27 12:35:29 +0900

    commit

--- 01_itp1/itp1_9_d_Transformation/src/main.rs
+++ 01_itp1/itp1_9_d_Transformation/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+  let s: Vec<String> = r();
+  let mut s = s[0].to_string();
+  let n: Vec<i32> = r();
+  for _ in 0..n[0] {
+    let v: Vec<String> = r();
+    let c: &str = &v[0];
+    let x: usize = v[1].parse().unwrap();
+    let y: usize = v[2].parse().unwrap();
+    match c {
+      "print" => { println!("{}", &s[x..y + 1]); }
+      "replace" => {
+        let z: &str = &v[3];
+        s.drain(x..y + 1);
+        s.insert_str(x, z);
+      }
+      "reverse" => {
+        let r = s.drain(x..y + 1).rev().collect::<String>();
+        s.insert_str(x, &r);
+      }
+      _ => {}
+    }
+  }
+}
+
+fn r<T: std::str::FromStr>() -> Vec<T> {
+  let mut b = String::new();
+  std::io::stdin().read_line(&mut b).unwrap();
+  b.split_whitespace().flat_map(str::parse).collect()
 }
