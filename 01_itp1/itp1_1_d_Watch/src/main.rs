41128794893108fdb916ddb8551aea9b21f22afc
eda3 <gerorin1010@gmail.com>
2020-08-27 12:56:42 +0900

    fix

--- 01_itp1/itp1_1_d_Watch/src/main.rs
+++ 01_itp1/itp1_1_d_Watch/src/main.rs
@@ -1,15 +1,11 @@
 fn main() {
-    let s = input();
-    let h = s / 3600;
-    let m = (s - h * 3600) / 60;
-    let s = s % 60;
-    println!("{}:{}:{}", h, m, s);
-}
-
-
-fn input() -> u32 {
-    let mut buf = String::new();
-    let stdin = std::io::stdin();
-    stdin.read_line(&mut buf).unwrap();
-    return buf.trim().parse().unwrap();
+  let s: i32 = {
+    let mut b = String::new();
+    std::io::stdin().read_line(&mut b).unwrap();
+    b.trim().parse().unwrap()
+  };
+  let h = s / 3600;
+  let m = (s - h * 3600) / 60;
+  let s = s % 60;
+  println!("{}:{}:{}", h, m, s);
 }
