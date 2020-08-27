96af4b078a156ab20e69521f29de23a3e1b784a1
eda3 <gerorin1010@gmail.com>
2020-08-27 21:05:20 +0900

    fix

--- 01_itp1/itp1_2_a_SmallLargeOrEqual/src/main.rs
+++ 01_itp1/itp1_2_a_SmallLargeOrEqual/src/main.rs
@@ -2,5 +2,5 @@ fn main() {
   let mut b = String::new();
   std::io::stdin().read_line(&mut b).unwrap();
   let v: Vec<i32> = b.split_whitespace().flat_map(str::parse).collect();
-  println!("{}", if v[0] < v[1] { "a < b" } else if v[0] > v[1] { "a > b" } else { "a == b" })
+  println!("a {} b", if v[0] < v[1] { "<" } else if v[0] > v[1] { ">" } else { "==" })
 }
